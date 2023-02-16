880027a41075bf9267b16857056a0b927416c439
DrFreeman1836 <98258060+DrFreeman1836@users.noreply.github.com>
2023-02-16 12:14:16 +0300

    Update DummyTest2.java

--- src/test/java/testftp/DummyTest2.java
+++ src/test/java/testftp/DummyTest2.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 public class DummyTest2 {
     @Test
     public void dummyTest1() throws InterruptedException {
-        Thread.sleep(5);
+        Thread.sleep(2500);
         Assertions.assertTrue(true);
     }
 
     @Test
     public void dummyTest2() throws InterruptedException {
-        Thread.sleep(5);
+        Thread.sleep(2500);
         Assertions.assertFalse(false);
     }
 }
