e510db2e2d0ffff397773124754ab0d38098d0e9
DrFreeman1836 <98258060+DrFreeman1836@users.noreply.github.com>
2023-02-17 15:50:15 +0300

    Update DummyTest.java

--- src/test/java/testftp/DummyTest.java
+++ src/test/java/testftp/DummyTest.java
@@ -8,7 +8,7 @@ public class DummyTest {
   @Test
   public void dummyTest1() throws InterruptedException {
     Thread.sleep(2500);
-    Assertions.assertTrue(true);
+    Assertions.assertTrue(false);
   }
 
   @Test
